cfcd512b8d604f2239504964de01b64111001e9b
Knut Nergaard <knut.nergaard@gmail.com>
2024-06-14 21:21:26 +0200

    Documentation changes, console-script entry points, requirements.txt

---
+++ bin/__init__.py
@@ -0,0 +1,5 @@
+from bin.calculateEngravingDefaults import calculateEngravingDefaults
+from bin.checkAnchors import checkAnchors
+from bin.generateMetadata import generateMetadata
+from bin.importAnchors import importAnchors
+from bin.importID import importID
