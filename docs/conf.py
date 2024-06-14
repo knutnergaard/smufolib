83300b669c6c53af7d3f51bdac2855755b6f0a93
Knut Nergaard <knut.nergaard@gmail.com>
2024-06-15 01:39:52 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,8 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.join(os.path.dirname((os.path.abspath('.')), 'smufolib', 'bin')
+
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
