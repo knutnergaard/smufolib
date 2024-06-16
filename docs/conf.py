d61f9a3d4f9e663c06aaf13815c3292883f69c32
Knut Nergaard <knut.nergaard@gmail.com>
2024-06-16 13:17:30 +0200

    updated conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -65,5 +65,5 @@ html_context = {'display_github': True,
                 'github_user': 'knutnergaard',
                 'github_repo': 'smufolib', }
 html_theme_options = {
-    'display_version': False,
+    'display_version': True,
 }
