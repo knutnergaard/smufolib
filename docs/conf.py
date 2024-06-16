45aaea580b13c69f61d61de2d0b4de42aaf7a637
Knut Nergaard <knut.nergaard@gmail.com>
2024-06-16 11:46:52 +0200

    updated docs/conf.py and pyproject.toml with versioning

--- docs/conf.py
+++ docs/conf.py
@@ -5,10 +5,9 @@
 
 import os
 import sys
-from pathlib import Path
+import importlib.metadata
 
 sys.path.insert(0, os.path.abspath('..'))
-
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -16,7 +15,7 @@ sys.path.insert(0, os.path.abspath('..'))
 project = 'SMufoLib'
 copyright = '2022, Knut Nergaard'
 author = 'Knut Nergaard'
-release = '0.1.0'
+release = importlib.metadata.version("smufolib")
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -24,11 +23,9 @@ release = '0.1.0'
 extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.autodoc',
-    # 'sphinx_autodoc_typehints',
     'sphinx.ext.doctest',
     'sphinx.ext.autosummary',
     'sphinx.ext.viewcode',
-
     'sphinx_rtd_theme',
 ]
 
@@ -38,24 +35,15 @@ intersphinx_mapping = {
     'setuptools': ('https://setuptools.readthedocs.io/en/latest', None)
 }
 
-
 # highlighting style
-pygments_style = 'sphinx'
+# pygments_style = 'sphinx'
 highlight_language = 'python3'
 
 # sphinx.ext.autodoc
 autodoc_typehints = 'description'
-# autodoc_typehints_description_target = 'all'
 autodoc_member_order = 'groupwise'
 autodoc_docstring_signature = True
 
-# sphinx_autodoc_typehints
-typehints_use_signature = False
-typehints_use_signature_return = False
-# always_document_param_types = False
-
-# spinx_paramlink
-# paramlinks_hyperlink_param = 'name'
 
 add_module_names = False
 
@@ -78,7 +66,4 @@ html_context = {'display_github': True,
                 'github_repo': 'smufolib', }
 html_theme_options = {
     'display_version': False,
-    # 'github_user': 'knutnergaard',
-    # 'github_repo': 'smufolib',
-    # 'github_button': 'true',
 }
