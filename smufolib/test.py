5eddfe859a042191d37836f38958c2b9ce7acb49
knutnergaard <knut.nergaard@gmail.com>
2025-03-25 13:17:14 +0100

    Add test suite executable.

---
+++ smufolib/test.py
@@ -0,0 +1,39 @@
+from fontParts.fontshell.component import RComponent
+from fontParts.fontshell.contour import RContour
+from smufolib.tests import testEnvironment
+from smufolib.objects.engravingDefaults import EngravingDefaults
+from smufolib.objects.font import Font
+from smufolib.objects.glyph import Glyph
+from smufolib.objects.layer import Layer
+from smufolib.objects.range import Range
+from smufolib.objects.smufl import Smufl
+from smufolib.request import Request
+
+
+classMapping = {
+    "component": RComponent,
+    "contour": RContour,
+    "engravingDefaults": EngravingDefaults,
+    "font": Font,
+    "glyph": Glyph,
+    "layer": Layer,
+    "range": Range,
+    "smufl": Smufl,
+    "request": Request,
+}
+
+
+def smufolibObjectGenerator(cls):
+    unrequested = []
+    obj = classMapping[cls]()
+    return obj, unrequested
+
+
+if __name__ == "__main__":
+    import sys
+
+    if {"-v", "--verbose"}.intersection(sys.argv):
+        verbosity = 2
+    else:
+        verbosity = 1
+    testEnvironment(smufolibObjectGenerator, verbosity=verbosity)
