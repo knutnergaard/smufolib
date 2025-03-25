53e7f8066c159a0874e1889cc6a5c376720c9c74
knutnergaard <knut.nergaard@gmail.com>
2025-03-25 17:39:59 +0100

    Add `__all__` and unused import ignore for `ruff`.

--- smufolib/__init__.py
+++ smufolib/__init__.py
@@ -1,7 +1,6 @@
-"""where SMuFL meets UFO
+"""where SMuFL meets UFO"""
 
-"""
-from smufolib._version import __version__
+from smufolib._version import __version__  # noqa: F401
 from smufolib.objects.engravingDefaults import EngravingDefaults
 from smufolib.objects.engravingDefaults import ENGRAVING_DEFAULTS_KEYS
 from smufolib.objects.font import Font
@@ -13,3 +12,17 @@ from smufolib.objects.smufl import ANCHOR_NAMES
 from smufolib.objects.smufl import FONT_ATTRIBUTES
 from smufolib.objects.smufl import GLYPH_ATTRIBUTES
 from smufolib.request import Request
+
+__all__ = [
+    "EngravingDefaults",
+    "ENGRAVING_DEFAULTS_KEYS",
+    "Font",
+    "Glyph",
+    "Layer",
+    "Range",
+    "Smufl",
+    "ANCHOR_NAMES",
+    "FONT_ATTRIBUTES",
+    "GLYPH_ATTRIBUTES",
+    "Request",
+]
