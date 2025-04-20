27205d3bfbf057d38127aaa74372290c28a20116
knutnergaard <knut.nergaard@gmail.com>
2025-04-20 09:22:20 +0200

    Update __init__.py with anchors and ANCHOR_NAMES location.

--- smufolib/__init__.py
+++ smufolib/__init__.py
@@ -3,14 +3,12 @@
 from smufolib._version import __version__  # noqa: F401
 from smufolib.objects.engravingDefaults import EngravingDefaults
 from smufolib.objects.engravingDefaults import ENGRAVING_DEFAULTS_KEYS
+from smufolib.objects.anchors import ANCHOR_NAMES, Anchors
 from smufolib.objects.font import Font
 from smufolib.objects.glyph import Glyph
 from smufolib.objects.layer import Layer
 from smufolib.objects.range import Range
-from smufolib.objects.smufl import Smufl
-from smufolib.objects.smufl import ANCHOR_NAMES
-from smufolib.objects.smufl import FONT_ATTRIBUTES
-from smufolib.objects.smufl import GLYPH_ATTRIBUTES
+from smufolib.objects.smufl import FONT_ATTRIBUTES, GLYPH_ATTRIBUTES, Smufl
 from smufolib.request import Request
 from smufolib.utils import converters
 from smufolib.utils import error
@@ -20,16 +18,16 @@ from smufolib.utils import stdUtils
 from smufolib.utils import scriptUtils
 
 __all__ = [
-    "EngravingDefaults",
+    "ANCHOR_NAMES",
     "ENGRAVING_DEFAULTS_KEYS",
+    "FONT_ATTRIBUTES",
+    "GLYPH_ATTRIBUTES",
+    "EngravingDefaults",
     "Font",
     "Glyph",
     "Layer",
     "Range",
     "Smufl",
-    "ANCHOR_NAMES",
-    "FONT_ATTRIBUTES",
-    "GLYPH_ATTRIBUTES",
     "Request",
     "converters",
     "error",
