12a4b9117e0d507a6e65c12367361c784d347b88
knutnergaard <knut.nergaard@gmail.com>
2025-05-04 02:53:28 +0200

    Add modules to __all__ and streamline imports.

--- smufolib/__init__.py
+++ smufolib/__init__.py
@@ -1,26 +1,36 @@
 """where SMuFL meets UFO"""
 
 from smufolib._version import __version__  # noqa: F401
-from smufolib.objects.engravingDefaults import EngravingDefaults
-from smufolib.objects.engravingDefaults import ENGRAVING_DEFAULTS_KEYS
+from smufolib import cli
+from smufolib import config
+from smufolib.objects.engravingDefaults import (
+    ENGRAVING_DEFAULTS_KEYS,
+    EngravingDefaults,
+)
 from smufolib.objects.font import Font
 from smufolib.objects.glyph import Glyph
 from smufolib.objects.layer import Layer
 from smufolib.objects.range import Range
-from smufolib.objects.smufl import Smufl
-from smufolib.objects.smufl import ANCHOR_NAMES
-from smufolib.objects.smufl import FONT_ATTRIBUTES
-from smufolib.objects.smufl import GLYPH_ATTRIBUTES
+from smufolib.objects.smufl import (
+    ANCHOR_NAMES,
+    FONT_ATTRIBUTES,
+    GLYPH_ATTRIBUTES,
+    Smufl,
+)
 from smufolib.request import Request
-from smufolib.utils import converters
-from smufolib.utils import error
-from smufolib.utils import normalizers
-from smufolib.utils import pointUtils
-from smufolib.utils import rulers
-from smufolib.utils import stdUtils
-from smufolib.utils import scriptUtils
+from smufolib.utils import (
+    converters,
+    error,
+    normalizers,
+    pointUtils,
+    rulers,
+    stdUtils,
+    scriptUtils,
+)
 
 __all__ = [
+    "cli",
+    "config",
     "EngravingDefaults",
     "ENGRAVING_DEFAULTS_KEYS",
     "Font",
