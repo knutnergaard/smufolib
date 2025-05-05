c8679e75571b50861f2a3e62f924a40d9b67f884
knutnergaard <knut.nergaard@gmail.com>
2025-05-05 14:50:28 +0200

    Add cited members to `Glyph`.

--- smufolib/objects/glyph.py
+++ smufolib/objects/glyph.py
@@ -1,6 +1,15 @@
 # pylint: disable=C0114
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
 from fontParts.fontshell.glyph import RGlyph
 from smufolib.objects.smufl import Smufl
+
+if TYPE_CHECKING:
+    from fontParts.fontshell.anchor import RAnchor
+    from fontParts.fontshell.contour import RContour
+    from fontParts.fontshell.component import RComponent
+    from fontParts.fontshell.guideline import RGuideline
 
 
 class Glyph(RGlyph):
@@ -15,7 +24,7 @@ class Glyph(RGlyph):
 
     """
 
-    def _set_name(self, value):
+    def _set_name(self, value: str) -> None:
         # Set the name of the glyph and update :attr:`.Font.lib` if necessary.
         if self.font is not None:
             namesDict = self.font.lib.get("com.smufolib.names", {})
@@ -28,3 +37,45 @@ class Glyph(RGlyph):
     def smufl(self) -> Smufl:
         """Glyph instance of :class:`~smufolib.objects.smufl.Smufl`."""
         return Smufl(glyph=self)
+
+    @property
+    def width(self) -> int | float:
+        """Proxy of :attr:`fontParts.base.BaseGlyph.width`."""
+        # pylint: disable=C2801
+        return RGlyph.width.__get__(self, type(self))
+
+    @width.setter
+    def width(self, value: int | float) -> None:
+        # pylint: disable=C2801
+        RGlyph.width.__set__(self, value)
+
+    @property
+    def height(self) -> int | float:
+        """Proxy of :attr:`fontParts.base.BaseGlyph.height`."""
+        # pylint: disable=C2801
+        return RGlyph.height.__get__(self, type(self))
+
+    @height.setter
+    def height(self, value: int | float) -> None:
+        # pylint: disable=C2801
+        RGlyph.height.__set__(self, value)
+
+    @property
+    def anchors(self) -> tuple[RAnchor, ...]:
+        """Proxy of :attr:`fontParts.base.BaseGlyph.anchors`."""
+        return super().anchors
+
+    @property
+    def contours(self) -> tuple[RContour, ...]:
+        """Proxy of :attr:`fontParts.base.BaseGlyph.contours`."""
+        return super().contours
+
+    @property
+    def components(self) -> tuple[RComponent, ...]:
+        """Proxy of :attr:`fontParts.base.BaseGlyph.components`."""
+        return super().components
+
+    @property
+    def guidelines(self) -> tuple[RGuideline, ...]:
+        """Proxy of :attr:`fontParts.base.BaseGlyph.guidelines`."""
+        return super().guidelines
