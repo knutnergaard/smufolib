8c5051728b54da5dddde32ff3946e22f1b52fec2
knutnergaard <knut.nergaard@gmail.com>
2025-05-06 11:47:42 +0200

    Implement wrapped object support and improve docs.

--- smufolib/objects/font.py
+++ smufolib/objects/font.py
@@ -1,16 +1,32 @@
 # pylint: disable=C0114
+import defcon
+from fontParts.base.font import BaseFont
 from fontParts.fontshell.font import RFont
 from smufolib.objects.layer import Layer
 from smufolib.objects.smufl import Smufl
+from smufolib.utils import error
 
 
 class Font(RFont):
-    """Environment implementation of :class:`fontParts.base.BaseFont`.
+    """SMufoLib environment implementation of :class:`fontParts.base.BaseFont`.
 
-    To import the :class:`Font` object and instantiate a font::
+    :param pathOrObject: The source used to initialize the font.
 
-        >>> from smufolib import Font
-        >>> font = Font('path/to/my/font.ufo')
+        - If :obj:`None`, a new, empty font will be created.
+        - If a :class:`str`, it is interpreted as a path to an existing UFO file.
+        - If an unwrapped native font object (e.g., a :class:`defcon.Font`) or a
+          :class:`fontParts.base.BaseFont`, it will be wrapped.
+
+        .. versionadded:: 0.6.0
+
+            Support for initializing from an existing :class:`fontParts.base.BaseFont`
+            instance.
+
+
+    Example::
+
+        >>> font = Font('path/to/font.ufo')     # Load from path
+        >>> font = Font(BaseFont())             # Wrap an existing font
 
     """
 
@@ -18,7 +34,17 @@ class Font(RFont):
 
     layerClass = Layer
 
+    def __init__(self, pathOrObject: str | defcon.Font | BaseFont | None = None):
+        if isinstance(pathOrObject, BaseFont):
+            try:
+                pathOrObject = pathOrObject.naked()
+            except AttributeError:
+                error.generateTypeError(
+                    pathOrObject, (BaseFont, defcon.Font), "pathOrObject"
+                )
+        super().__init__(pathOrObject)
+
     @property
     def smufl(self) -> Smufl:
-        """Font instance of :class:`~smufolib.objects.smufl.Smufl`."""
+        """Font-specific instance of :class:`~smufolib.objects.smufl.Smufl`."""
         return Smufl(font=self)
